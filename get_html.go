1da63fedf6d40fe8bbb1529ff682edf7405214e9
ExgleGaming <167655468+ExgleGaming@users.noreply.github.com>
2025-04-11 00:03:41 -0400

    added functionality to get html from a site

---
+++ get_html.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func getHTML(rawURL string) (string, error) {
+	res, err := http.Get(rawURL)
+	if err != nil {
+		return "", fmt.Errorf("failed to fetch HTML: %v", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode > 399 {
+		log.Fatalf("Response failed with status code: %d", res.StatusCode)
+	}
+
+	contentType := res.Header.Get("Content-Type")
+	if !strings.Contains(contentType, "text/html") {
+		return "", fmt.Errorf("unexpected content type: %s", contentType)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read HTML body: %v", err)
+	}
+
+	htmlBody := string(body)
+
+	return htmlBody, nil
+}
--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
-const htmlBody = `
-			<html>
-				<body>
-					<a href="/path/one">
-						<span>Boot.dev</span>
-					</a>
-					<a href="https://other.com/path/one">
-						<span>Boot.dev</span>
-					</a>
-				</body>
-			</html>
-			`
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	getURLsFromHTML(htmlBody, "https://blog.boot.dev")
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+	if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	baseURL := os.Args[1]
+
+	fmt.Printf("starting crawl of: %v...\n", baseURL)
+	htmlBody, err := getHTML(baseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(htmlBody)
 }
